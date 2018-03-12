bd44d659e77752b86aab1fa421891825b8914b78
alfandjacky <37285747+alfandjacky@users.noreply.github.com>
2018-03-12 20:44:58 +0800

    Update main.go

--- main.go
+++ main.go
@@ -27,7 +27,7 @@ var bot *linebot.Client
 func main() {
 	var err error
 	bot, err = linebot.New(os.Getenv("ChannelSecret"), os.Getenv("ChannelAccessToken"))
-	san :=fmt.Println(rand.Intn(100))
+	san := rand.Intn(100)
 	linebot.NewTextMessage(san)
 	log.Println("Bot:", bot, " err:", err)
 	http.HandleFunc("/callback", callbackHandler)
