0c5091fd9c38974043087d07c057291cf5965a3a
alfandjacky <37285747+alfandjacky@users.noreply.github.com>
2018-03-12 21:20:41 +0800

    Update main.go

--- main.go
+++ main.go
@@ -54,7 +54,8 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 		if event.Type == linebot.EventTypeMessage {
 			switch message := event.Message.(type) {
 			case *linebot.TextMessage:
-				if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.ID+":"+message.Text+str1)).Do(); err != nil {
+				if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.ID+":"+message.Text+str1)).Do()
+				err != nil {
 					log.Print(err)
 				}
 			}
